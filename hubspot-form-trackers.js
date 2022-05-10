73006a677f985a2a6c941f65cf11c480d17e181c
marcinem <marcinmichalak.m@gmail.com>
2022-05-10 12:22:29 +0200

    final fix

--- hubspot-form-trackers.js
+++ hubspot-form-trackers.js
@@ -221,42 +221,21 @@ window.addEventListener("message", (event) => {
       });
     }
     function addEventId() {
-      function generateUUID() {
-        var d = new Date().getTime(); //Timestamp
-        var d2 =
-          (typeof performance !== "undefined" &&
-            performance.now &&
-            performance.now() * 1000) ||
-          0; //Time in microseconds since page-load or 0 if unsupported
-        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-          /[xy]/g,
-          function (c) {
-            var r = Math.random() * 16; //random number between 0 and 16
-            if (d > 0) {
-              //Use timestamp until depleted
-              r = (d + r) % 16 | 0;
-              d = Math.floor(d / 16);
-            } else {
-              //Use microseconds since page-load if supported
-              r = (d2 + r) % 16 | 0;
-              d2 = Math.floor(d2 / 16);
-            }
-            return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-          }
-        );
-      }
+      var fbEventId = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
+        /[018]/g,
+        (c) =>
+          (
+            c ^
+            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+          ).toString(16)
+      );
 
       document
         .querySelectorAll('[name="facebook_event_id"]')
         .forEach(function (field) {
-          field.value = generateUUID();
-          console.log(generateUUID());
+          field.value = fbEventId;
+          console.log(fbEventId);
         });
-      window.dataLayer.push({
-        "event": "hubspot-form-loaded",
-        'hs-form-guid': event.data.id,
-        'hs-form-event-id': generateUUID()
-      });
     }
     function addExperimentTracking() {
       var propertyId = "UA-81405772-1";
